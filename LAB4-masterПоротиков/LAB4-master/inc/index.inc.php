9aff18011a9a0a89d26fd16a3cbac9c3b9773fdc
Porotikov <72504468+Porotikov@users.noreply.github.com>
2020-10-09 00:56:19 +0300

    Add files via upload

---
+++ LAB4-masterПоротиков/LAB4-master/inc/index.inc.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewpoint" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 		<title>php</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+
+	<body>
+		<header>
+			
+
+		<div class="container slider">
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  				<div class="carousel-inner">
+    			<div class="carousel-item active">
+     			 <img src="img/main.jpg" class="d-block w-100" alt="...">
+    			</div>
+   				 <div class="carousel-item">
+   				 	<img src="img/mem.jpg" class="d-block w-100" alt="...">
+    			</div>
+  				</div>
+  				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    			<span class="sr-only">Previous</span>
+ 				 </a>
+  				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    			<span class="sr-only">Next</span>
+  				</a>
+			</div>
+		</div>
+		<div class="container">
+						<p>Abibas(не Adidas)<br>
+
+						Abibas-фирменный магазин одежды для парней с района. Открыты с 2014 в Севастополе и с 2019 в Москве,приобретайте наш мерч.
+						
+						Открытие происходило по просьбе ребят с района,разрабатывали первый дизайн вещей на примере кампании Adidas от модельера GlebProduction в его сити.
+						
+						Общежитие номер 2 СевГУ стало местом зарождения нашей культуры!
+
+						<br>
+
+			</p>
+		</div>
+
+		<footer class="page-footer font-small unique-color-dark pt-0">
+			<div class="primary-color">
+				<div class="container">
+					<div class="row py-4 d-flex align-items-center">
+						<div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+							<h6 class="mb-0 white-text">Get connected with us on social networks!</h6>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</footer>
+	</body>	
+
+</html>	
+ 
